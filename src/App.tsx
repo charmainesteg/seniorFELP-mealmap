7570261e09f0e583e96ddcb16de4b40b7ddcab96
charmaine.stegall <charmaine.stegall@ey.com>
2023-05-03 16:24:42 -0400

    Finished routing for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import {Routes, Route, Router} from "react-router-dom"
 import './App.css';
 import Header from './Components/PageContainer/Header';
 import Footer from './Components/PageContainer/Footer';
+import HomePage from './Components/Pages/HomePage';
+import Pantries from './Components/Pages/Pantries';
+import Resources from './Components/Pages/Resources';
+import About from './Components/Pages/About';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      {/* <div className="testdiv">Testing...</div> */}
+        <Routes>
+          <Route index element = {<HomePage/>}/>
+          <Route path="/pantries" element = {<Pantries/>}/>
+          <Route path="/resources" element = {<Resources/>}/>
+          <Route path="/about" element = {<About/>}/>
+        </Routes>
       <Footer/>
     </div>
   );
