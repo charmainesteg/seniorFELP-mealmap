0da340952c7a00c252f2241f1509400676b87092
charmaine.stegall <charmaine.stegall@ey.com>
2023-05-04 10:25:17 -0400

    fixing code mistakes

--- src/Components/Cards/PantryCard.tsx
+++ src/Components/Cards/PantryCard.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Card } from "react-bootstrap";
 
 export default function PantryCard({title,text}: {title:string, text: string}){
-    // const title = props.title
-    // const text = props.text
     return (
         <Card style={{ width: '18rem' }}>
           <Card.Img variant="top" src="holder.js/100px180" />
