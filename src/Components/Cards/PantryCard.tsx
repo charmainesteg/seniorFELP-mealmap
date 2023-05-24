924141afa61beca09a13ef3cfc855b3d7857020a
charmaine.stegall <charmaine.stegall@ey.com>
2023-05-24 11:38:15 -0400

    created type for pantry props

--- src/Components/Cards/PantryCard.tsx
+++ src/Components/Cards/PantryCard.tsx
@@ -1,20 +1,30 @@
 import { Card } from "react-bootstrap";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-export default function PantryCard({id, title,description, image, pantrylocation, url}: {id: number, title:string, description: string, image: string, pantrylocation: string, url: string}){
-    return (
-        <Card style={{ width: '18rem' }} key={id}>
-          <Card.Img variant="top" src={image} />
-          <Card.Body>
-              <Card.Title> <Link to={`/pantries/${id}`}> {title}</Link> </Card.Title>
-            <Card.Text>
-              {description}
-            </Card.Text>
-            <Card.Text>
-              {pantrylocation}
-            </Card.Text>
-            <Card.Link href={url} target="_blank">Find more info here</Card.Link>
-          </Card.Body>
-        </Card>
-      );
+export type PantryProps = {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  pantrylocation: string;
+  url: string;
+};
+
+export default function PantryCard(props: PantryProps) {
+  return (
+    <Card style={{ width: "18rem" }} key={props.id}>
+      <Card.Img variant="top" src={props.image} />
+      <Card.Body>
+        <Card.Title>
+          {" "}
+          <Link to={`/pantries/${props.id}`}> {props.title}</Link>{" "}
+        </Card.Title>
+        <Card.Text>{props.description}</Card.Text>
+        <Card.Text>{props.pantrylocation}</Card.Text>
+        <Card.Link href={props.url} target="_blank">
+          Find more info here
+        </Card.Link>
+      </Card.Body>
+    </Card>
+  );
 }
