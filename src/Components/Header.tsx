ac1363939a9c06b70e80f311e3012b97a334648c
charmaine.stegall <charmaine.stegall@ey.com>
2023-05-25 10:06:48 -0400

    Completed search function

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -11,14 +11,14 @@ export default function Header() {
   return (
     <div>
       <Navbar className="navbar-container" expand="lg">
-        <Navbar.Brand className="brand-name" as={NavLink} to="/">
+      <Navbar.Brand className="brand-name" as={NavLink} to="/">
           <h3>
             <span>MEAL</span> Map
           </h3>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
+        <Navbar.Collapse id='navbarScroll'>
+        <Nav
             className="me-auto my-2 my-lg-0"
             style={{ maxHeight: "100px" }}
             navbarScroll
@@ -32,6 +32,6 @@ export default function Header() {
           <SearchPantry/>
         </Navbar.Collapse>
       </Navbar>
-    </div>
+      </div>
   );
 }
--- src/Components/SearchPantry.tsx
+++ src/Components/SearchPantry.tsx
@@ -1,3 +1,4 @@
+import { render } from "@testing-library/react";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
@@ -6,10 +7,19 @@ import "./SearchPantry.css";
 
 function SearchPantry() {
   const [inputText, setInputText] = useState("");
+
   let inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     var lowerCase = e.target.value.toLowerCase();
     setInputText(lowerCase);
   };
+
+  const renderSearchList = () => {
+    if(inputText){
+        return <SearchList input={inputText} />;
+    }
+    return null;
+  }
+
 
   return (
     <div className="search-pantry-div">
@@ -24,7 +34,7 @@ function SearchPantry() {
         <Button className="search-button">Search</Button>
       </Form>
       <div className="search-list">
-        <SearchList input={inputText} />
+        {renderSearchList()}
       </div>
     </div>
   );
