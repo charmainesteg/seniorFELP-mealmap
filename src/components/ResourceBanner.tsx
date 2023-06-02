7229d68c8bc0fb5c3338bb5528d4a9f2e2e4d8c8
charmaine.stegall <charmaine.stegall@ey.com>
2023-06-02 14:42:48 -0400

    Completed Resource Banner Component

---
+++ src/components/ResourceBanner.tsx
@@ -0,0 +1,45 @@
+import "../pages/Resources.css"
+
+const ResourceBanner = () =>{
+    return(
+        <div className="resource-banner">
+<h3 className="banner-header">
+  To find more information click the icons:
+</h3>
+<div className="row">
+  <div className="resource-items snap col-sm">
+    <a
+      href="https://www.fns.usda.gov/snap/supplemental-nutrition-assistance-program"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <i className="fa-solid fa-utensils"></i>
+    </a>
+    <h4>SNAP</h4>
+  </div>
+  <div className="resource-items tefap col-sm">
+    <a
+      href="https://dfcs.georgia.gov/services/food-commodity-programs/emergency-food-assistance-program"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <i className="fa-solid fa-bread-slice"></i>
+    </a>
+    <h4>TEFAP</h4>
+  </div>
+  <div className="resource-items csfp col-sm">
+    <a
+      href="https://dfcs.georgia.gov/services/food-commodity-programs/commodity-supplemental-food-program"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <i className="fa-solid fa-seedling"></i>
+    </a>
+    <h4>CSFP</h4>
+  </div>
+</div>
+</div>
+    )
+}
+
+export default ResourceBanner;
--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -1,6 +1,7 @@
 import ResourceCard from "../components/cards/ResourceCard";
 import resourceData from "../assets/data/resourceData";
 import AddResourceCard from "../components/cards/AddResourceCard";
+import ResourceBanner from "../components/ResourceBanner";
 import addresourceimg from "../assets/images/addresourceimg.jpeg";
 import "./Resources.css";
 
@@ -23,43 +24,7 @@ const Resources = () => {
           />
         ))}
       </div>
-      <div className="resource-banner">
-        <h3 className="banner-header">
-          To find more information click the icons:
-        </h3>
-        <div className="row">
-          <div className="resource-items snap col-sm">
-            <a
-              href="https://www.fns.usda.gov/snap/supplemental-nutrition-assistance-program"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-solid fa-utensils"></i>
-            </a>
-            <h4>SNAP</h4>
-          </div>
-          <div className="resource-items tefap col-sm">
-            <a
-              href="https://dfcs.georgia.gov/services/food-commodity-programs/emergency-food-assistance-program"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-solid fa-bread-slice"></i>
-            </a>
-            <h4>TEFAP</h4>
-          </div>
-          <div className="resource-items csfp col-sm">
-            <a
-              href="https://dfcs.georgia.gov/services/food-commodity-programs/commodity-supplemental-food-program"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-solid fa-seedling"></i>
-            </a>
-            <h4>CSFP</h4>
-          </div>
-        </div>
-      </div>
+    <ResourceBanner/>
       <div className="additional-resources row">
         <div className="add-resource-item col-sm">
           <AddResourceCard />
