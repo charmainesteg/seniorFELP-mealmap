8b6a9c5874f76e2383afe776975b43b2537aa396
charmaine.stegall <charmaine.stegall@ey.com>
2023-06-06 10:56:02 -0400

    added more resources to the additional resources section

--- src/assets/data/addResourcesData.tsx
+++ src/assets/data/addResourcesData.tsx
@@ -8,9 +8,22 @@ const addResourcesData = [
       url: "https://dfcs.georgia.gov/services/low-income-home-energy-assistance-program-liheap"
     },
     {
-      title: "Temporary Assistance for Needy Families",
+      title: "Temporary Assistance for Needy Families (TANF)",
       url: "https://dfcs.georgia.gov/services/temporary-assistance-needy-families"
-    }
+    },
+    {
+        title: "State of Georgia Post-Secondary Tuition Waiver",
+        url: "https://dfcs.georgia.gov/services/state-georgia-post-secondary-tuition-waiver"
+    },
+    {
+        title: "Childcare and Parent Services (CAPS)",
+        url: "https://caps.decal.ga.gov/en/About"
+    },
+    {
+        title: "Georgia C.R.E.W (Cultivating the Rising Experienced Worker)",
+        url: "https://dfcs.georgia.gov/services/georgia-crew-cultivating-rising-experienced-worker"
+    },
+
   ];
 
   export default addResourcesData
