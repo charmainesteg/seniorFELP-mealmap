1cf78a3321f2f569ab9cbf466735b06b4b53b83b
charmaine.stegall <charmaine.stegall@ey.com>
2023-06-07 12:06:52 -0400

    updated styling for pantry

--- src/pages/PantryDetail.tsx
+++ src/pages/PantryDetail.tsx
@@ -1,5 +1,5 @@
 import { useParams, useNavigate } from "react-router-dom";
-import {Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import pantryData from "../assets/data/pantryData";
 import "./PantryDetail.css"
 
@@ -20,33 +20,37 @@ const PantryDetail = () => {
 
   return (
     <div className="pantry-detail-container">
-        <Button onClick={goBack}>Back</Button>
-          <div className="container" key={pantry.id}>
-      <div className="row">
-        <div className="col-md-5">
-          <div className="box">
-            <h1 className="pantry-detail-title">{pantry.title}</h1>
-            <p className="pantry-detail-description">{pantry.description}</p>
+      <Button onClick={goBack}>Back</Button>
+      <div className="container pantry-detail" key={pantry.id}>
+        <div className="row">
+          <div className="col-md-5">
+            <div className="box">
+              <h1 className="pantry-detail-title">{pantry.title}</h1>
+              <p className="pantry-detail-description">{pantry.description}</p>
+            </div>
+            <div className="box">
+              <p>Hours: {pantry.hours}</p>
+              <p>Additional Info: {pantry.additionalDetails}</p>
+            </div>
+            <div className="box">
+              <p className="pantry-detail-location">{pantry.location}</p>
+              <a className="pantry-detail-url" href={pantry.url} target="_blank" rel="noreferrer">
+                Find more here
+              </a>
+            </div>
           </div>
-          <div className="box">
-            <p className="pantry-detail-location">{pantry.location}</p>
-            <a className="pantry-detail-url" href={pantry.url}>
-              Find more here
-            </a>
-          </div>
-        </div>
 
-        <div className="col-md-7">
-          <img
-            src={pantry.image}
-            alt={pantry.title}
-            className="pantry-detail-img"
-          ></img>
+          <div className="col-md-7">
+            <img
+              src={pantry.image}
+              alt={pantry.title}
+              className="pantry-detail-img"
+            ></img>
+          </div>
         </div>
       </div>
     </div>
-    </div>
-  
+
   );
 };
 
